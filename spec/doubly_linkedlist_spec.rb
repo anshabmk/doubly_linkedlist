d5f5665cad0e080226070aa421cb50561ebdac3c
Anshab M K <anshabmk@live.com>
2020-05-01 21:46:39 +0530

    Adds tests for array.to_list method

--- spec/doubly_linkedlist_spec.rb
+++ spec/doubly_linkedlist_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe DoublyLinkedlist do
   it "requires doubly_linkedlist/list" do
     expect(DoublyLinkedlist::List).not_to be nil
   end
+
+  context "Conversion from Array" do
+    it "should monkey patch to_list method into Array class" do
+      expect(Array.new.methods.include?(:to_list)).to be true
+    end
+
+    it "should convert an array into a doubly linkedlist" do
+      random_arr = Array.new(Random.rand(10), Random.rand(100))
+      list = random_arr.to_list
+
+      expect(list.class).to eql(DoublyLinkedlist::List)
+    end
+  end
 end
